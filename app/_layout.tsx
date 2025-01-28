426e52a5a22a292e814cbe3f0d1874b4346253c4
dixitpriyanshu <priyanshu.dixit.2910@gmail.com>
2025-01-28 11:58:57 +0530

    WIP

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
